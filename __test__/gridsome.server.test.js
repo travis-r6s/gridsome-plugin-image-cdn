117c90f5ea37a2b2e4f401826b69789f78b59cc4
Kyle Welsby <kyle@mekyle.com>
2021-02-04 17:21:38 +0000

    🚨 Add linting to CI

--- __test__/gridsome.server.test.js
+++ __test__/gridsome.server.test.js
@@ -1,107 +1,106 @@
-const server = require("../gridsome.server");
+const server = require('../gridsome.server')
 
 const transformerFn = jest.fn(({ cdn, sourceUrl, args }) => {
-  return cdn.baseUrl + cdn.imagePrefix + sourceUrl;
-});
+  return cdn.baseUrl + cdn.imagePrefix + sourceUrl
+})
 
 const customTransformer = {
   createSchemaTypes: jest.fn(),
   createResolverArgs: jest.fn(),
   transformer: transformerFn
-};
+}
 
-function setup(config, data) {
+function setup (config, data) {
   return new Promise((resolve, reject) => {
-
-
     const options = Object.assign(
       {
         site: {},
         cdn: {
-          baseUrl: "http://example.com",
+          baseUrl: 'http://example.com',
           transformer: customTransformer,
-          imagePrefix: "/"
+          imagePrefix: '/'
         },
         types: [
           {
-            typeName: "Thing",
-            sourceField: "field"
+            typeName: 'Thing',
+            sourceField: 'field'
           }
         ]
       },
       config
-    );
-    const addSchemaResolvers = jest.fn((cb) => {
-      const responses = [];
-      options.types.forEach((type) => {
-        const resp = cb[type.typeName][type.sourceField].resolve(
-          data[type.typeName],
+    )
+    const addSchemaResolvers = jest.fn(cb => {
+      const responses = []
+      options.types.forEach(type => {
+        const resp = cb[ type.typeName ][ type.sourceField ].resolve(
+          data[ type.typeName ],
           {},
           {},
           {
             path: {
-              key: ""
+              key: ''
             }
           }
-        );
-        responses.push(resp);
-      });
-      resolve(responses);
-    });
+        )
+        responses.push(resp)
+      })
+      resolve(responses)
+    })
 
-    const loadSource = jest.fn((cb) => {
-      cb({
+    const loadSource = jest.fn(cb => {
+      const ctx = {
         addSchemaTypes: jest.fn(),
         schema: {
           createEnumType: jest.fn()
         },
         addSchemaResolvers
-      });
-    });
+      }
+      cb(ctx)
+    })
     const api = {
       loadSource
-    };
-    server(api, options);
-  });
+    }
+    server(api, options)
+  })
 }
 
-test("resolves the full custom CDN URL", async () => {
+test('resolves the full custom CDN URL', async () => {
   const responses = await setup(
     {
       cdn: {
-        baseUrl: "http://example.com",
+        baseUrl: 'http://example.com',
         transformer: customTransformer,
-        imagePrefix: "/"
+        imagePrefix: '/'
       },
       types: [
         {
-          typeName: "Post",
-          sourceField: "featured"
+          typeName: 'Post',
+          sourceField: 'featured'
         }
       ]
     },
     {
       Post: {
-        featured: "image.jpg"
+        featured: 'image.jpg'
       }
     }
-  );
+  )
 
-  expect(responses[0]).toEqual("http://example.com/image.jpg");
-});
+  expect(responses[ 0 ]).toEqual('http://example.com/image.jpg')
+})
 
-test.skip("resolves null as the orignal value", async () => { // Issue https://github.com/thetre97/gridsome-plugin-image-cdn/issues/20
+test.skip('resolves null as the orignal value', async () => { // Issue https://github.com/thetre97/gridsome-plugin-image-cdn/issues/20
   const responses = await setup(
     {
       cdn: {
-        baseUrl: "http://example.com",
+        baseUrl: 'http://example.com',
         transformer: customTransformer,
-        imagePrefix: "/"
+        imagePrefix: '/'
       },
       types: [
         {
-          typeName: "Post",
-          sourceField: "featured"
+          typeName: 'Post',
+          sourceField: 'featured'
         }
       ]
     },
@@ -110,7 +109,7 @@ test.skip("resolves null as the orignal value", async () => { // Issue https://g
         featured: null
       }
     }
-  );
+  )
 
-  expect(responses[0]).toEqual(null);
-});
+  expect(responses[ 0 ]).toEqual(null)
+})
